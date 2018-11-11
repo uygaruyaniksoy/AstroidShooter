c52178b15f69de80c7bc86aae1c4c844f2e2bf7d
murat <e203601@metu.edu.tr>
2018-11-11 15:04:31 +0300

    Players enttity added

---
+++ src/main/java/com/group26/termproject/Players.java
@@ -0,0 +1,21 @@
+package com.group26.termproject;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+
+public class Players {
+    private @Id @GeneratedValue Long id;
+    private String nick_name;
+    private String email;
+    private String password;
+
+    Players(String nick_name, String email,String password ) {
+        this.nick_name = nick_name;
+        this.email = email;
+        this.password = password;
+    }
+}
