6156a546b9f26fad0058bf31beefac58fd7437bf
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2018-11-11 20:37:47 +0300

    refactored main class

---
+++ src/main/java/com/group26/termproject/CommandLineRunner.java
@@ -0,0 +1,12 @@
+package com.group26.termproject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
--- src/main/java/com/group26/termproject/TermProjectApplication.java
+++ src/main/java/com/group26/termproject/TermProjectApplication.java
@@ -1,14 +1,7 @@
 package com.group26.termproject;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 @SpringBootApplication
 public class TermProjectApplication {
@@ -18,22 +11,4 @@ public class TermProjectApplication {
 	}
 }
 
-@RestController
-class PlayerRestController {
-	@RequestMapping("/test")
-	Collection<Object> testQuery() {
-		ArrayList<Object> objects = new ArrayList<>();
-		return objects;
-	}
-}
 
-
-@Component
-class CommandLineRunner implements org.springframework.boot.CommandLineRunner {
-
-
-	@Override
-	public void run(String... args) throws Exception {
-
-	}
-}
