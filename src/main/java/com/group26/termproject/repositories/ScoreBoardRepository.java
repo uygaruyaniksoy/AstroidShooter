caa43e2c0e83ee12688883772ec1781d7c293aed
murat <e203601@metu.edu.tr>
2018-11-12 16:20:40 +0300

    Get services of LeaderBoard are ready. Not tested much

--- src/main/java/com/group26/termproject/repositories/ScoreBoardRepository.java
+++ src/main/java/com/group26/termproject/repositories/ScoreBoardRepository.java
@@ -2,6 +2,20 @@ package com.group26.termproject.repositories;
 
 import com.group26.termproject.tables.ScoreBoard;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import javax.persistence.Tuple;
+import java.util.List;
 
 public interface ScoreBoardRepository extends JpaRepository<ScoreBoard, Long> {
+
+    @Query("select s.player.nickName, max(score) from ScoreBoard s where  s.date=CURRENT_DATE group by s.player.nickName")
+    List<Tuple> getDailyLeaderboard();
+
+
+
+    @Query("select s.player.nickName, max(score) from ScoreBoard s " +
+            "where WEEK(s.date)=WEEK(CURRENT_DATE) and YEAR(CURRENT_DATE)=YEAR(s.date) " +
+            "group by s.player.nickName")
+    List<Tuple> getWeeklyLeaderboard();
 }
--- src/main/java/com/group26/termproject/tables/ScoreBoard.java
+++ src/main/java/com/group26/termproject/tables/ScoreBoard.java
@@ -1,28 +1,29 @@
 package com.group26.termproject.tables;
 
-
-import com.group26.termproject.tables.Player;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.sql.Date;
 
-@Data
 @Entity
+@Data
 public class ScoreBoard {
-    private @Id @GeneratedValue Long id;
-    private Long playerId;
-    private Date date_time;
+    private @Id @GeneratedValue Integer sid;
+    @ManyToOne
+    @JoinColumn(name = "pid",nullable = false)
+    private Player player;
+    private Date date;
     private int score;
 
-    public ScoreBoard() {
+    ScoreBoard() {
+        player = new Player();
+        date = null;
+        score = -1;
     }
 
-    ScoreBoard(Player player, Date date_time, int score) {
-        this.playerId = player.getId();
-        this.date_time = date_time;
+    public ScoreBoard(Player player, Date date, int score) {
+        this.player = player;
+        this.date = date;
         this.score = score;
     }
 }
