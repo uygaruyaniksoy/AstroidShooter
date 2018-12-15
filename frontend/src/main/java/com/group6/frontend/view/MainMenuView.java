d46817023247b61bba7fc79883fafec8633534d7
murat <e203601@metu.edu.tr>
2018-12-15 12:26:35 +0300

    Main Menu View are added the tabs

--- frontend/src/main/java/com/group6/frontend/view/MainMenuView.java
+++ frontend/src/main/java/com/group6/frontend/view/MainMenuView.java
@@ -1,9 +1,17 @@
 package com.group6.frontend.view;
 
 import com.group6.frontend.controller.MainMenuViewController;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MainMenuView extends AbstractView {
@@ -17,10 +25,35 @@ public class MainMenuView extends AbstractView {
     @Override
     public Scene getScene() {
         Scene scene = super.getScene();
+//        Pane root = (Pane) scene.getRoot();
+//        Button gameButton = new Button("Game");
+//        gameButton.setOnMouseClicked(controller::gameButtonHandler);
+
+
+//        root.getChildren().add(gameButton);
+
         Pane root = (Pane) scene.getRoot();
-        Button gameButton = new Button("Game");
-        gameButton.setOnMouseClicked(controller::gameButtonHandler);
-        root.getChildren().add(gameButton);
+
+        TabPane tabPane = new TabPane();
+
+        BorderPane borderPane = new BorderPane();
+        for (int i = 0; i < 2; i++) {
+            Tab tab = new Tab();
+            tab.setText("Tab" + i);
+            tab.setClosable(false);
+            HBox hbox = new HBox();
+            hbox.getChildren().add(new Label("Tab" + i));
+            hbox.setAlignment(Pos.CENTER);
+            tab.setContent(hbox);
+            tabPane.getTabs().add(tab);
+        }
+        // bind to take available space
+        borderPane.prefHeightProperty().bind(scene.heightProperty());
+        borderPane.prefWidthProperty().bind(scene.widthProperty());
+
+        borderPane.setCenter(tabPane);
+        root.getChildren().add(borderPane);
+
 
         return scene;
     }
