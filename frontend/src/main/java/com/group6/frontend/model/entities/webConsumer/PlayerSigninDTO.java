2b3d3dc47203f02e4fe8ed73a76dd87731871ef8
murat <e203601@metu.edu.tr>
2018-12-16 13:54:58 +0300

    A small change in PlayerSignInDTO

--- frontend/src/main/java/com/group6/frontend/model/entities/webConsumer/PlayerSigninDTO.java
+++ frontend/src/main/java/com/group6/frontend/model/entities/webConsumer/PlayerSigninDTO.java
@@ -1,14 +1,10 @@
 package com.group6.frontend.model.entities.webConsumer;
 
+
 import lombok.Data;
 
 @Data
 public class PlayerSignInDTO {
     private String email;
     private String password;
-
-    public PlayerSignInDTO( String email, String password) {
-        this.email = email;
-        this.password = password;
-    }
 }
