c37b291533e124f307ea12fa0f99720afc991d91
Uygar Uyanıksoy <uygaruyaniksoy@gmail.com>
2019-01-16 22:11:12 +0300

    executabls

--- backend-server/src/main/java/com/group6/backendserver/Server.java
+++ backend-server/src/main/java/com/group6/backendserver/Server.java
@@ -5,8 +5,6 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.concurrent.atomic.AtomicInteger;
 
 class Server {
